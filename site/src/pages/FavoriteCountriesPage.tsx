c5205df985f9d4ee0befa2fc37d2448cb60a9263
Vitor Mateus <vitor7jan@gmail.com>
2025-05-26 19:32:07 -0300

    showing favorite countries

--- site/src/pages/FavoriteCountriesPage.tsx
+++ site/src/pages/FavoriteCountriesPage.tsx
@@ -1,3 +1,33 @@
+import { useEffect, useState } from "react";
+import { FavoriteCountry, getFavoriteCountries } from "../services/api";
+
 export const FavoriteCountriesPage = () => {
-  return <div>FavoriteCountriesPage</div>;
+  const [favoriteCountries, setFavoriteCountries] = useState<FavoriteCountry[]>(
+    []
+  );
+
+  useEffect(() => {
+    (async () => {
+      const output = await getFavoriteCountries();
+      if (output.error) return alert("Error to get favorite countries.");
+      setFavoriteCountries(output.data);
+    })();
+  }, []);
+
+  return (
+    <div>
+      <h1>Favorite Countries</h1>
+
+      <ul>
+        {favoriteCountries.map((country) => (
+          <li>
+            <div>
+              <span>Cioc</span>
+              <strong>{country.cioc}</strong>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
