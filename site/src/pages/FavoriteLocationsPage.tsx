862a71ad0288a680e976b02760a39b7a5c62e090
Vitor Mateus <vitor7jan@gmail.com>
2025-05-26 19:34:38 -0300

    showing favorite locations

--- site/src/pages/FavoriteLocationsPage.tsx
+++ site/src/pages/FavoriteLocationsPage.tsx
@@ -1,3 +1,38 @@
+import { useEffect, useState } from "react";
+import { FavoriteLocation, getFavoriteLocations } from "../services/api";
+
 export const FavoriteLocationsPage = () => {
-  return <div>FavoriteLocationsPage</div>;
+  const [favoriteLocations, setFavoriteLocations] = useState<
+    FavoriteLocation[]
+  >([]);
+
+  useEffect(() => {
+    (async () => {
+      const output = await getFavoriteLocations();
+      if (output.error) return alert("Error to get favorite locations.");
+      setFavoriteLocations(output.data);
+    })();
+  }, []);
+
+  return (
+    <div>
+      <h1>Favorite Locations</h1>
+
+      <ul>
+        {favoriteLocations.map((location) => (
+          <li>
+            <div>
+              <span>Latitude</span>
+              <span>{location.lat}</span>
+            </div>
+
+            <div>
+              <span>Longitue</span>
+              <span>{location.lon}</span>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
