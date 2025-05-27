b2478b3e0cff4d886a27acf6ff9a16a02b8526a1
Vitor Mateus <vitor7jan@gmail.com>
2025-05-27 01:54:34 -0300

    implemented requests to api

--- site/src/services/api.ts
+++ site/src/services/api.ts
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
 const API_URL = process.env.REACT_APP_API_URL;
 if (!API_URL) throw Error("missing API_URL environment variable");
@@ -50,116 +50,115 @@ export type LoginInput = {
   password: string;
 };
 
+const handleSuccess = (res: AxiosResponse<any, any>) => ({
+  error: false,
+  data: res.data,
+});
+
+const handleError = (error: any) => ({
+  error: true,
+  data: error?.response?.data?.apiErrorMessage ?? "Unknown error.",
+});
+
+const getHeaders = (accessToken: string) => ({
+  Authorization: `Bearer ${accessToken}`,
+});
+
+const handleRequest = (request: Promise<AxiosResponse<any, any>>) =>
+  request.then(handleSuccess).catch(handleError);
+
 export const api = {
   getCountryDemographicInformation: async (
     cioc: string
   ): Promise<Output<Country>> => {
-    return {
-      error: false,
-      data: {
-        commonName: "Brazil",
-        officialName: "Federative Republic of Brazil",
-        cioc: "BRA",
-        population: 212559409,
-        languages: ["Portuguese"],
-        currencies: [{ symbol: "R$", name: "Brazilian real" }],
-      },
-    };
+    return await handleRequest(httpClient.get(`/countries/${cioc}`));
   },
 
   getLocationWeatherInformation: async (
     lat: number,
     lon: number
   ): Promise<Output<Location>> => {
-    return {
-      error: false,
-      data: {
-        currentUTCTimeUnix: 1748294616,
-        temperatureInCelsius: 21.35,
-        feelsLikeInCelsius: 21.26,
-        pressureInhPa: 1022,
-        humidityPercent: 66,
-        cloudsPercent: 0,
-        windSpeedInMeterPerSecond: 2.57,
-      },
-    };
+    return await handleRequest(
+      httpClient.get(`/locations/lat/${lat}/lon/${lon}`)
+    );
   },
 
-  getFavoriteCountries: async (): Promise<Output<FavoriteCountry[]>> => {
-    return {
-      error: false,
-      data: [
-        {
-          id: "568d91ac-7cfd-4584-85bb-bb4cdad365f4",
-          cioc: "GBR",
-        },
-      ],
-    };
+  getFavoriteCountries: async (
+    accessToken: string
+  ): Promise<Output<FavoriteCountry[]>> => {
+    return await handleRequest(
+      httpClient.get(`/favorite-countries`, {
+        headers: getHeaders(accessToken),
+      })
+    );
   },
 
-  getFavoriteLocations: async (): Promise<Output<FavoriteLocation[]>> => {
-    return {
-      error: false,
-      data: [
-        {
-          id: "f41a2d76-ebd4-4e94-baa9-00fe033a2801",
-          lat: 90,
-          lon: -107,
-        },
-      ],
-    };
+  getFavoriteLocations: async (
+    accessToken: string
+  ): Promise<Output<FavoriteLocation[]>> => {
+    return await handleRequest(
+      httpClient.get(`/favorite-locations`, {
+        headers: getHeaders(accessToken),
+      })
+    );
   },
 
-  createFavoriteCountry: async (input: {
-    cioc: string;
-  }): Promise<Output<{ favoriteCountryId: string }>> => {
-    return {
-      error: false,
-      data: {
-        favoriteCountryId: "f41a2d76-ebd4-4e94-baa9-00fe033a2801",
-      },
-    };
+  createFavoriteCountry: async (
+    input: {
+      cioc: string;
+    },
+    accessToken: string
+  ): Promise<Output<{ favoriteCountryId: string }>> => {
+    return await handleRequest(
+      httpClient.post(`/favorite-countries`, input, {
+        headers: getHeaders(accessToken),
+      })
+    );
   },
 
-  removeFavoriteCountry: async (id: string): Promise<Output<void>> => {
-    return {
-      error: false,
-      data: undefined,
-    };
+  removeFavoriteCountry: async (
+    id: string,
+    accessToken: string
+  ): Promise<Output<void>> => {
+    return await handleRequest(
+      httpClient.delete(`/favorite-countries/${id}`, {
+        headers: getHeaders(accessToken),
+      })
+    );
   },
 
-  createFavoriteLocation: async (input: {
-    lat: number;
-    lon: number;
-  }): Promise<Output<{ favoriteLocationId: string }>> => {
-    return {
-      error: false,
-      data: {
-        favoriteLocationId: "f41a2d76-ebd4-4e94-baa9-00fe033a2802",
-      },
-    };
+  createFavoriteLocation: async (
+    input: {
+      lat: number;
+      lon: number;
+    },
+    accessToken: string
+  ): Promise<Output<{ favoriteLocationId: string }>> => {
+    return await handleRequest(
+      httpClient.post(`/favorite-locations`, input, {
+        headers: getHeaders(accessToken),
+      })
+    );
   },
 
-  removeFavoriteLocation: async (id: string): Promise<Output<void>> => {
-    return {
-      error: false,
-      data: undefined,
-    };
+  removeFavoriteLocation: async (
+    id: string,
+    accessToken: string
+  ): Promise<Output<void>> => {
+    return await handleRequest(
+      httpClient.delete(`/favorite-locations/${id}`, {
+        headers: getHeaders(accessToken),
+      })
+    );
   },
 
   signup: async (input: SignupInput): Promise<Output<void>> => {
-    return {
-      error: false,
-      data: undefined,
-    };
+    return await handleRequest(httpClient.post(`/users`, input));
   },
 
   login: async (
     input: LoginInput
   ): Promise<Output<{ accessToken: string }>> => {
-    return {
-      error: false,
-      data: { accessToken: "any-jwt" },
-    };
+    return await handleRequest(httpClient.post(`/login`, input));
   },
 };
